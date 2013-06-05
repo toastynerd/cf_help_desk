cebaa0f77ab5631461c99e67c88f55147609b382
tylermorgan86 <tylermorgan86@gmail.com>
2013-06-05 12:20:23 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Code Fellows Help Desk
+=============================
+An app a day for learning action mailer in
+the form of a help desk app.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelpDesk::Application.config.secret_token = '60f6e8121c512d603e5f2bf35b1f0785a68ba0d008ea18de44a8e5e0859b501bcf0eb677d95fb05ba49df617f31da58f8d3179d587c6aed81f51ecc24ca2cd38'
